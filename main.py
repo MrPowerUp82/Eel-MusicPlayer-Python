5173935346f69e2edfa284808039c4478eaf6093
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2022-01-15 03:41:32 -0300

    push

---
+++ main.py
@@ -0,0 +1,57 @@
+from distutils.log import error
+import eel
+import os
+from utils import play_sound, pause_sounds, unpause_sounds, stop_sounds
+
+eel.init('web')
+
+@eel.expose()
+def next(path,sounds, atual):
+    stop_sounds()
+    idx = sounds.index(atual)
+    idx_next = idx +1
+    n = len(sounds)-1
+    if idx_next >= n:
+        play_sound(path+'\\'+sounds[0])
+        return sounds[0]    
+    else:
+        play_sound(path+'\\'+sounds[idx_next])
+        return sounds[idx_next]
+
+@eel.expose()
+def prev(path,sounds, atual):
+    stop_sounds()
+    idx = sounds.index(atual)
+    idx_prev = idx -1
+    n = len(sounds)-1
+    if idx_prev <= -1:
+        play_sound(path+'\\'+sounds[-1])
+        return sounds[-1]    
+    else:
+        play_sound(path+'\\'+sounds[idx_prev])
+        return sounds[idx_prev]
+
+                        
+@eel.expose
+def unpause():
+    return unpause_sounds()
+
+@eel.expose
+def pause():
+    return pause_sounds()
+
+@eel.expose
+def get_username():
+    return os.environ.get('USERNAME')
+
+@eel.expose
+def get_musics(path):
+    return [x for x in os.listdir(path) if '.mp3' in x]
+
+@eel.expose
+def play(path,sounds):
+    play_sound(path+'\\'+sounds[0])
+    return sounds[0]
+
+
+eel.start('index.html', size=(400,650))
---
+++ utils.py
@@ -0,0 +1,23 @@
+from pygame import mixer
+
+mixer.init()
+
+def play_sound(path):
+    mixer.music.load(path)
+    mixer.music.play()
+
+
+def stop_sounds():
+    mixer.music.stop()
+    
+def pause_sounds():
+    mixer.music.pause()
+    
+def unpause_sounds():
+    mixer.music.unpause()
+    
+def is_sound_working():
+    if mixer.music.get_busy() == True:
+        return True
+    
+    return False
