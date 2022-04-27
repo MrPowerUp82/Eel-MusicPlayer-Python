75732c5cbc6d84798e55db27f7abd5f7822f93ce
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2022-04-27 17:17:13 -0300

    refactoring

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-import threading
-from pygame import mixer
 import audio_metadata
 from time import strftime
 from time import gmtime
@@ -15,44 +13,14 @@ def get_art(sound,path_art):
         arq.write(art)
     return f'temp_art.png'
 
-mixer.init()
-
-def play_sound(path):
-    mixer.music.load(path)
-    mixer.music.play()
-
-
-def stop_sounds():
-    mixer.music.stop()
-    
-def pause_sounds():
-    mixer.music.pause()
-    
-def unpause_sounds():
-    mixer.music.unpause()
-    
-def get_volume():
-    return mixer.music.get_volume()
-
-def set_volume(vol):
-    return mixer.music.set_volume(vol)
-    
-def get_pos():
-    time_int = mixer.music.get_pos()/1000
-    time = strftime("%M:%S", gmtime(int(time_int)))
-    return [time_int,time]
     
 def get_end(file):
     meta = audio_metadata.load(file)['streaminfo']['duration']
     time =  strftime("%M:%S", gmtime(int(meta)))
     return [meta,time]
     
-def set_pos(value):
-    mixer.music.set_pos(value)
-    return False
 
-def is_working():
-    n = mixer.music.get_busy()
-    if n >= 1:
-        return True
-    return False
+def get_time(timer):
+    time =  strftime("%M:%S", gmtime(int(timer)))
+    return [timer, time]
+
