5dd8a1465c609f3fab0aa754e0c03bc4fff81e57
MrPowerUp82 <gustavogostozo8@gmail.com>
2022-11-14 03:01:19 -0300

    update

--- utils.py
+++ utils.py
@@ -2,13 +2,23 @@ import audio_metadata
 from time import strftime
 from time import gmtime
 import os
+from functools import lru_cache
+from unicodedata import normalize
 
+def tirar_acento(text):
+    clean_text = normalize('NFKD',text).encode('ASCII','ignore').decode('ASCII')
+    return clean_text
+
+@lru_cache
 def search_for_musics()->str:
-    for root, dir, files in os.walk(os.path.expanduser('~')):
-        if len([x for x in files if x == 'temp.mp3']):
-            continue
-        elif len([x for x in files if x.endswith('.mp3')]):
-            return root + '/'
+    user_path = os.path.expanduser('~')
+    possibles_dirs = [user_path+'/'+x for x in os.listdir(user_path) if os.path.isdir(user_path+'/'+x) and (tirar_acento(x).lower().startswith('downloa') or tirar_acento(x).lower().startswith('music'))]
+    for possible_dir in possibles_dirs:
+        for root, dir, files in os.walk(possible_dir):
+            if len([x for x in files if x == 'temp.mp3']):
+                continue
+            elif len([x for x in files if x.endswith('.mp3')]):
+                return root + '/'
 
 
 def get_art(sound,path_art):
