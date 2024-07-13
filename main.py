a152eb9e9d37bc8dccd6745fcc145cdbdbce0957
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2024-07-13 14:37:14 -0300

    Refactor code to use tuple instead of list for file extensions

--- main.py
+++ main.py
@@ -5,22 +5,9 @@ from utils import get_art, get_time, search_for_musics
 import tkinter 
 import tkinter.filedialog as filedialog
 
-# if not sys.platform == 'win32':
-#     actual_path = os.getcwd()
-#     path = os.path.join('/tmp/')
-#     os.chdir(path)
-#     if not os.path.exists('pysound'):
-#         os.mkdir('pysound')
-    
-#     os.chdir(actual_path)
-
-
-
 eel.init('web')
 
-ext_musics = [
-    'mp3', 'wav','ogg','wma','aac']
-
+ext_musics = ('mp3', 'wav','ogg','wma','aac')
 
 @eel.expose
 def select_path():
@@ -30,7 +17,6 @@ def select_path():
     directory_path = filedialog.askdirectory()
     return directory_path+'/'
 
-
 @eel.expose
 def time_f(time):
     return get_time(time)[1]
@@ -38,13 +24,10 @@ def time_f(time):
 @eel.expose
 def start():
     return search_for_musics()
-    # user_path = os.path.expanduser('~')
-    # path_music = [x for x in os.listdir(user_path) if 'Musi' in x or 'Músi' in x][0]
-    # return user_path+'/'+(path_music if path_music else 'Music')+'/'
 
 @eel.expose
 def get_musics(path):
-    music_list =[x for x in os.listdir(path) if x.split('.')[-1].lower() in ext_musics]
+    music_list =[x for x in os.listdir(path) if x.endswith(ext_musics)]
     return music_list
 
 @eel.expose
@@ -53,14 +36,6 @@ def create_file(music):
     if 'main.py' in sys.argv[0]:
         path = os.getcwd()+'/web/'
     else:
-        # if sys.platform == 'win32':
-        #     # path = os.path.expanduser('~')+'/AppData/Local/Temp/pysound/'
-        #     path = os.path.join(temp_dir, 'pysound') + '/'
-        # else:
-        #     # if not os.path.exists('/tmp/pysound'):
-        #     #     os.mkdir('/tmp/pysound')
-        #     # path = '/tmp/pysound/'
-        #     path = os.path.join(temp_dir, 'pysound') + '/'
         path =eel.root_path + '/'
     with open(f'{path}temp.{ext}', 'wb') as arq:
         music_arq = open(music, 'rb').read()
--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import os
 from functools import lru_cache
 from unicodedata import normalize
 
+ext_musics = ('mp3', 'wav','ogg','wma','aac')
+
 def tirar_acento(text):
     clean_text = normalize('NFKD',text).encode('ASCII','ignore').decode('ASCII')
     return clean_text
@@ -15,13 +17,11 @@ def search_for_musics()->str:
     possibles_dirs = [user_path+'/'+x for x in os.listdir(user_path) if os.path.isdir(user_path+'/'+x) and (tirar_acento(x).lower().startswith('downloa') or tirar_acento(x).lower().startswith('music'))]
     for possible_dir in possibles_dirs:
         for root, dir, files in os.walk(possible_dir):
-            if len([x for x in files if x == 'temp.mp3']):
-                continue
-            elif len([x for x in files if x.endswith('.mp3')]):
-                return root + '/'
+            if any([x for x in root.split(os.path.sep) if x.startswith('.')]): continue
+            if len([x for x in files if x == 'temp.mp3']): continue
+            if len([x for x in files if x.endswith(ext_musics)]): return root + '/'
 
     return user_path
-
 
 def get_art(sound,path_art):
     try:
@@ -32,13 +32,11 @@ def get_art(sound,path_art):
     with open(f'{path_art}temp_art.png', 'wb') as arq:
         arq.write(art)
     return f'temp_art.png'
-
     
 def get_end(file):
     meta = audio_metadata.load(file)['streaminfo']['duration']
     time =  strftime("%M:%S", gmtime(int(meta)))
     return [meta,time]
-    
 
 def get_time(timer):
     time =  strftime("%M:%S", gmtime(int(timer)))
